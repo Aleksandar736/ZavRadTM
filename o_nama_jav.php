ef0dad1443a5f5ead9d540407505983368f58b6e
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-11-30 15:34:05 +0100

    Create o_nama_jav.php

---
+++ o_nama_jav.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/stil.css">
+    <title>O aplikaciji</title>
+</head>
+<body>
+    <?php
+        include 'header.php';
+    ?>    
+    <div class="raspored-kolona">
+        <div class="navigacija-sa-leve-strane">
+            
+        </div>
+        
+        <div class="glavni-sadrzaj">
+            <div class="glavni-naslov">
+                <h1>Tim Menadžment</h1>
+            </div>
+        
+            <div class="tekst-sadrzaja">
+                <div class="oAplikaciji">                      
+                    <h3 class="dodavanjeNaslov">O aplikaciji Upravljanje Timovima</h3>
+                    <p>
+                        Aplikacija Upravljanje Timovima je Web aplikacija, koju  
+                        koriste tim menadžeri za manipulaciju timovima na projektima 
+                        u projektno orijentisanoj organizaciji. Korisnici sistema 
+                        su menadžeri, tim lideri i obični članovi tima. Menadžer 
+                        ima mogućnost kreiranja projekata, kreiranja timova za pojedine 
+                        projekte, dodavanja zaposlenih koji će biti raspoređeni u 
+                        timove, dodeljivanje uloge tim lidera i menadžera, kao i pregled 
+                        izveštaja. Tim lider određuje konkretne zadatke članova tima. 
+                        Dok obični član izveštava o % izvršenosti svog zadatka.
+                    </p>
+                    <p>
+                        Aplikacija sadrži korisnički interfejs za tri korisničke role (uloge):
+                    </p>
+                        <ol>
+                            <li>Menadžer</li>
+                            <li>Tim lider</li>
+                            <li>Obični član tima</li>
+                        </ol>
+                        
+                </div>              
+            </div>        
+        </div>
+        
+        <div class="navigacija-sa-desne-strane">
+            
+        </div>
+    </div>
+    <?php
+        include 'footer.php';
+    ?>    
+</body>
+</html>
+
