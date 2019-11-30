4ba59cf3d23d5de81aff8fcb4f7383efd07f2ec7
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-11-30 14:22:57 +0100

    Create bris_e_pisma_clan.php

---
+++ bris_e_pisma_clan.php
@@ -0,0 +1,12 @@
+<?php
+include("sesija_clana.php");
+
+$id_por = $_REQUEST['key'];
+include 'konekcija.php';
+
+$upit = "DELETE FROM `slanje_poruka` WHERE `idporuke` = '".$id_por."'";
+mysqli_query($kon_sa_serv, $upit);
+mysqli_close($kon_sa_serv);
+
+echo "<script>location.href = 'primlj_e_pisma_clan.php'</script>";
+?>
