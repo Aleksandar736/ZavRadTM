2c1a614dc27063e32b4f54bcb4ac6f2a72eac41b
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-11-30 15:47:46 +0100

    Create pretr_liste_kor.php

---
+++ pretr_liste_kor.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'konekcija.php';
+
+if(isset($_REQUEST["q"])){
+$q = $_REQUEST["q"];    
+
+echo "<div>";
+echo "<table border = '2' width = '85%'>";
+echo "<tr>";
+echo "<th>Korisničko ime</th>";
+echo "<th>Tim</th>";
+echo "</tr>";
+
+$upit = "SELECT * FROM `ucesce` WHERE `korisnicko_ime` LIKE '%".$q."%'";
+$result = mysqli_query($kon_sa_serv, $upit);
+while ($red = mysqli_fetch_assoc($result)) {
+        $kor_im = $red['korisnicko_ime'];
+        $im_ti = $red['ime_tima'];
+        
+        echo "<tr>";
+        echo "<td>".ucfirst($kor_im)."</td>";
+        echo "<td>".ucfirst($im_ti)."</td>";
+        echo "</tr>";
+    }
+}
+?>
+
