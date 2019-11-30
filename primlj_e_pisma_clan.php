ed5c9fa58ff9006caa32a0dc0bdb5ff4a8963e07
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-11-30 15:52:57 +0100

    Create primlj_e_pisma_clan.php

---
+++ primlj_e_pisma_clan.php
@@ -0,0 +1,117 @@
+<?php
+include("sesija_clana.php");
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/stil.css">
+    <title>Poruke (član tima)</title>
+</head>
+<body>
+    <?php
+        include 'heder_obi_clana.php';
+    ?>     
+    <div class="raspored-kolona">
+        <div class="navigacija-sa-leve-strane">
+            <table>
+                <tr>
+                    <td>
+                        <a href ="obicni_clan.php"><button>Glavna stranica člana tima</button></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href ="prihvat_zadatka_clan.php"><button>Projekti</button></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href = "e_posta_clan.php"><button>Pošta</button></a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        
+        <div class="glavni-sadrzaj">
+            <div class="glavni-naslov">
+                <h1>Tim Menadžment</h1>
+            </div>
+        
+            <div class="dodavanjeNaslov">
+                <h3>Poštansko sanduče:</h3>
+                
+                <div class="tekst-sadrzaja">
+                <table border = "2" width = "90%">
+                    <tr>
+                        <th>Pregled poruka</th>
+                        <th>Šalje</th>
+                        <th>Naslov</th>
+                        <th>Datum slanja</th>
+                        <th>Odgovor / Brisanje</th>
+                    </tr>
+
+                    <?php
+                    include 'konekcija.php';
+
+                    $upit = "SELECT * FROM `slanje_poruka` WHERE `prima` = '".$pri_kor."' ORDER BY `datum_slanja` DESC";
+                    $result = mysqli_query($kon_sa_serv, $upit);
+                    while ($red = mysqli_fetch_assoc($result)) {
+                        $id_por = $red['idporuke'];
+                        $preg = $red['pregledana'];
+                        $salje = $red['salje'];
+                        $nasl_por = $red['naslov_poruke'];
+                        $dat_sl = $red['datum_slanja'];
+                        echo "<tr>";
+                        if($preg == 0){
+                            echo "<td align = 'center' width = '1'><a href = 'citanj_e_pisma_clan.php?key=".$id_por."'><img src = 'images/unopened-mail.png' border = '0'></img></a></td>";
+                        }
+                        else{
+                            echo "<tr><td align = 'center' width = '1'><a href = 'citanj_e_pisma_clan.php?key=".$id_por."'><img src = 'images/openmail.png' border = '0'></img></a></td>";
+                        }
+                        echo "<td align = 'center'><b>".ucfirst($salje)."</b></td>";
+                        if($nasl_por == ""){
+                            echo "<td align = 'center' width = '150'>bez naslova</td>";
+                        }
+                        else{
+                            echo "<td align = 'center' width = '150'>".ucfirst($nasl_por)."</td>";
+                        }
+                        echo "<td align = 'center'>$dat_sl</td>";
+                        echo "<td align = 'center' width ='75'><a href = 'odg_na_e_pismo_clan.php?key=".$salje."'><img src = 'images/mail_reply.png' border = '0'></img></a>";
+                        echo " "."<a href = 'bris_e_pisma_clan.php?key=".$id_por."'><img src = 'images/mail_delete.png' border = '0'></img></a></td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </table>
+                </div>
+            </div>                       
+            
+        </div>
+                    
+        <div class="navigacija-sa-desne-strane">
+            <table>
+                <tr>
+                    <td>            
+                        <a href = "pis_e_pisma_cla.php"><button>Napiši <br/>e-pismo</button></a>
+                    </td>
+                </tr>            
+                <tr>
+                    <td>
+                        <a href = "primlj_e_pisma_clan.php"><button>Primljena <br/>e-pisma</button></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href = "poslata_e_pisma_clan.php"><button>Poslata <br/>e-pisma</button></a>
+                    </td>
+                </tr>                
+            </table>
+        </div>
+    </div>
+    <?php
+        include 'footer.php';
+    ?> 
+</body>
+</html>
+
