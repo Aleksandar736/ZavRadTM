ad5a5cb46c95bb275e0dcc1339b9a2e4abd39a09
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-11-30 14:24:55 +0100

    Create bris_e_pisma_po_men.php

---
+++ bris_e_pisma_po_men.php
@@ -0,0 +1,13 @@
+<?php
+include("sesija_menadzera.php");
+
+$id_por = $_REQUEST['key'];
+include 'konekcija.php';
+
+$upit = "DELETE FROM `slanje_poruka` WHERE `idporuke` = '".$id_por."'";
+mysqli_query($kon_sa_serv, $upit);
+mysqli_close($kon_sa_serv);
+
+echo "<script>location.href = 'poslata_e_pisma_men.php'</script>";
+?>
+
