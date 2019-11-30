73b5f6aca3d54937598239fb4ec83e0e9d9a5203
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-11-30 15:34:36 +0100

    Create o_nama_men.php

---
+++ o_nama_men.php
@@ -0,0 +1,78 @@
+<?php
+include("sesija_menadzera.php");
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/stil.css">
+    <title>O aplikaciji (menadžer)</title>
+</head>
+<body>
+    <?php
+        include 'heder_menadzera.php';
+    ?>     
+    <div class="raspored-kolona">
+        <div class="navigacija-sa-leve-strane">
+            <table>
+                <tr>
+                    <td>
+                        <a href ="menadzer.php"><button>Glavna stranica menadžera</button></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href ="upravljacka_stranica.php"><button>Upravljačka stranica</button></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href ="projekti_po_timovima.php"><button>Projekti po timovima</button></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href ="e_posta_men.php"><button>Pošta</button></a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="glavni-sadrzaj">
+            <div class="glavni-naslov">
+                <h1>Tim Menadžment</h1>
+            </div>
+        
+            <div class="tekst-sadrzaja">
+                <div class="oAplikaciji">
+                    <h3 class="dodavanjeNaslov">O menadžeru</h3>
+                    <p>Korisnik u ulozi menadžera ima sledeće mogućnosti:
+                        <ol>
+                            <li>Prijava korisnika;</li>
+                            <li>Registracija članova tima;</li>
+                            <li>Promena svoje lozinke;</li>
+                            <li>Promena zaboravljene lozinke članova tima;</li>
+                            <li>Unos / izmena / pretraga / brisanje timova;</li>
+                            <li>Unos / izmena / pretraga / brisanje projekata;</li>
+                            <li>Unos / izmena / pretraga / brisanje članova tima na projektu;</li>
+                            <li>Dodeljivanje uloge tim lidera nekom od članova tima na projektu;</li>
+                            <li>Dodeljivanje uloge menadžera;</li>
+                            <li>Pregled timova po projektima;</li>
+                            <li>Pregled učešća na projektima po članu;</li>
+                            <li>Evidentiranje završetka projekta i oslobađanje timova.</li>
+                        </ol>
+                    </p>
+                </div>
+            </div>
+            
+        </div>
+        <div class="navigacija-sa-desne-strane">
+            
+        </div>
+    </div>
+    <?php
+        include 'footer.php';
+    ?>     
+</body>
+</html>
+
