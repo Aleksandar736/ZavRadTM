4040dc8b88fee2777b375522aedeea17e5d06670
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-11-30 15:31:04 +0100

    Create heder_menadzera.php

---
+++ heder_menadzera.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="navbar-collapse d-flex flex-row" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item hover-pocetne">
+        <a class="nav-link" href="o_nama_men.php">O menadžeru</a>
+      </li>
+    </ul>
+                 
+    <ul class="navbar-nav navbar-right">
+      <li class="nav-item active hover-pocetne">
+          <a class="nav-link" href="index.php">Izloguj se</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
