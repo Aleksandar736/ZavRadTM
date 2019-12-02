22bb7f0dd4bdeaf1134b482d56a827c9358afd2f
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-12-02 05:52:46 +0100

    Update citanj_po_e_pisma_clan.php

--- citanj_po_e_pisma_clan.php
+++ citanj_po_e_pisma_clan.php
@@ -43,11 +43,11 @@ include("sesija_clana.php");
                 <?php
                 $id_por = $_REQUEST['key'];
                 include 'konekcija.php';
-
+                //Namesta u bazi na 1 (umesto 0) u koloni pregl._po (pa se broj neproč. smanjuje za jedan (brojač ga ne broji))
                 $upit = "UPDATE `slanje_poruka` SET `pregledana_po` = 1 WHERE `idporuke` = '".$id_por."'";
                 mysqli_query($kon_sa_serv, $upit);
 
-
+                //Čita iz baze poruku sa id-jem $id_por
                 $upit = "SELECT * FROM `slanje_poruka` WHERE `idporuke` = '".$id_por."'";
                 $result = mysqli_query($kon_sa_serv, $upit);
                 while ($red = mysqli_fetch_assoc($result)) {
