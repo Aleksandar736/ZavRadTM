5fa722b4ada2c94f232eeff590b970e45eea14ff
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-12-02 06:15:40 +0100

    Update pretr_liste_kor.php

--- pretr_liste_kor.php
+++ pretr_liste_kor.php
@@ -15,13 +15,13 @@ echo "</tr>";
 $upit = "SELECT * FROM `ucesce` WHERE `korisnicko_ime` LIKE '%".$q."%'";
 $result = mysqli_query($kon_sa_serv, $upit);
 while ($red = mysqli_fetch_assoc($result)) {
-        $kor_im = $red['korisnicko_ime'];
-        $im_ti = $red['ime_tima'];
-        
-        echo "<tr>";
-        echo "<td>".ucfirst($kor_im)."</td>";
-        echo "<td>".ucfirst($im_ti)."</td>";
-        echo "</tr>";
+    $kor_im = $red['korisnicko_ime'];
+    $im_ti = $red['ime_tima'];
+
+    echo "<tr>";
+    echo "<td>".ucfirst($kor_im)."</td>";
+    echo "<td>".ucfirst($im_ti)."</td>";
+    echo "</tr>";
     }
 }
 ?>
