0681b6fd27c9eb3574054c99ce7e5cd01bee2af9
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-12-02 06:27:30 +0100

    Update upravljacka_stranica.php

--- upravljacka_stranica.php
+++ upravljacka_stranica.php
@@ -45,7 +45,7 @@ include("sesija_menadzera.php");
                 </tr>
                 <tr>
                     <td>
-                        <a href ="projekti_po_timovima.php"><button>Projekti</button></a>
+                        <a href ="projekti_po_timovima.php"><button>Projekti po timovima</button></a>
                     </td>
                 </tr>
                 <tr>
@@ -61,67 +61,61 @@ include("sesija_menadzera.php");
                 <h1>Tim Menadžment</h1>
             </div>
         
-            <div class="tekst-sadrzaja">
+            <div class="tekst-sadrzaja oAplikaciji">
                 <div class="tabelaProPo">
                     <h3 class="dodavanjeNaslov">Korisnici:</h3>
                 </div>
 
                 <div class="tekst-sadrzaja">
-                    <div>
+                    <div class="tekst-sadrzaja">
                         <form>
                         Traženi korisnici: <input type="text" onkeyup="pretraga(this.value)">
                         </form>
                         <p>Pronađeni korisnici:<br/> <span id="trazeni"></span></p>
                     </div>
-                <div class="tekst-sadrzaja">
-                    <div class="tabelaProPo">
-                        <h3 class="dodavanjeNaslov">Svi korisnici:</h3>
+                    <div class="tekst-sadrzaja">
+                        <div class="tabelaProPo">
+                            <h3 class="dodavanjeNaslov">Svi korisnici:</h3>
+                        </div>
+                        <table border = "2" width = "90%">
+                        <tr>
+                            <th>Ime korisnika</th>
+                            <th>Ime tima</th>
+                            <th>Uloga u timu</th>
+                            <th>Brisanje <br/>/ Izmene</th>
+                        </tr>
+                        <?php
+                        include 'konekcija.php';
+                        //Iz tabele ucesce uzimamo kor. ime, ime tima, ulogu i ime projekta
+                        $upit = "SELECT * FROM `ucesce` ORDER BY `ime_tima`, `uloga`, `korisnicko_ime`, `ime_projekta` ASC";
+                        $result = mysqli_query($kon_sa_serv, $upit);
+                        while ($red = mysqli_fetch_assoc($result)) {
+                            $kor_iz_baz = $red['korisnicko_ime'];
+                            $ime_tima = $red['ime_tima'];
+                            $poz = $red['uloga'];
+                            $ime_proje = $red['ime_projekta'];
+                            //Prvo ide ime
+                            echo "<tr><td><b>".ucfirst($kor_iz_baz)."</b></td>";
+                            if($ime_tima == ""){//Ako nema tima ide van tima
+                                echo "<td><b><font color = 'red'>van tima</font></b></td>";
+                            }
+                            else{//Ako ga ima ide njegovo ime
+                                echo "<td>".ucfirst($ime_tima)."</td>";
+                            }//Ako je pozicija tim lider to se ispisuje
+                            if($poz == "tim_lider"){
+                                echo "<td><font color = 'red'><b>$poz</b></font></td>";
+                            }
+                            else{//I kad nije tim lider ispisuje se uloga
+                                echo "<td>$poz</td>";
+                            }
+                            echo "<td width = '70px'><a href = 'brisanje_iz_tima.php?key=".$kor_iz_baz."'><img src = 'images/list_remove_user.png' alt = 'delete' border = '0'></img></a>";
+                            echo "<a href = 'prerasporedje_kor.php?key=".$kor_iz_baz."'><img src = 'images/005-pen_1.png' border = '0' alt = 'edit'></img></a>";
+                        }
+                        mysqli_close($kon_sa_serv);
+
+                        ?>
+                        </table>
                     </div>
-                    <table border = "2" width = "90%">
-                    <tr>
-                        <th>Ime korisnika</th>
-                        <th>Ime tima</th>
-                        <th>Ime projekta</th>
-                        <th>Uloga u timu</th>
-                        <th>Brisanje <br/>/ Izmene</th>
-                    </tr>
-                    <?php
-                    include 'konekcija.php';
-
-                    $upit = "SELECT * FROM `ucesce` ORDER BY `ime_tima`, `uloga`, `korisnicko_ime`, `ime_projekta` ASC";
-                    $result = mysqli_query($kon_sa_serv, $upit);
-                    while ($red = mysqli_fetch_assoc($result)) {
-                        $kor_iz_baz = $red['korisnicko_ime'];
-                        $ime_tima = $red['ime_tima'];
-                        $poz = $red['uloga'];
-                        $ime_proje = $red['ime_projekta'];
-                        echo "<tr><td><b>".ucfirst($kor_iz_baz)."</b></td>";
-                        if($ime_tima == ""){
-                            echo "<td><b><font color = 'red'>van tima</font></b></td>";
-                        }
-                        else{
-                            echo "<td>".ucfirst($ime_tima)."</td>";
-                        }
-                        if($ime_proje == ""){
-                            echo "<td><b><font color = 'red'>bez projekta</font></b></td>";
-                        }
-                        else{
-                            echo "<td>".ucfirst($ime_proje)."</td>";
-                        }
-                        if($poz == "tim_lider"){
-                            echo "<td><font color = 'red'><b>$poz</b></font></td>";
-                        }
-                        else{
-                            echo "<td>$poz</td>";
-                        }
-                        echo "<td width = '70px'><a href = 'oduz_ul_timl_men.php?key=".$kor_iz_baz."'><img src = 'images/list_remove_user.png' alt = 'delete' border = '0'></img></a>";
-                        echo "<a href = 'prerasporedje_kor.php?key=".$kor_iz_baz."'><img src = 'images/005-pen_1.png' border = '0' alt = 'edit'></img></a>";
-                    }
-                    mysqli_close($kon_sa_serv);
-
-                    ?>
-                    </table>
-                </div>
                 </div>
             </div>
         </div>
