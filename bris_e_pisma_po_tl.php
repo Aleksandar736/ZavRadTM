0bef414f105031bb332b0783f3aed78baf8058c1
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-12-02 05:46:21 +0100

    Update bris_e_pisma_po_tl.php

--- bris_e_pisma_po_tl.php
+++ bris_e_pisma_po_tl.php
@@ -8,8 +8,9 @@ $upit = "DELETE FROM `slanje_poruka` WHERE `idporuke` = '".$id_por."'";
 mysqli_query($kon_sa_serv, $upit);
 mysqli_close($kon_sa_serv);
 
-echo "<script>location.href = 'primlj_e_pisma_tl.php'</script>";
+echo "<script>location.href = 'poslata_e_pisma_tl.php'</script>";
 ?>
+
 
 
 
