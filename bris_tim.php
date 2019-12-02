03b5a98c8e393c4c4c4cb1ce7e7e008d25264b2f
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-12-02 05:50:02 +0100

    Update bris_tim.php

--- bris_tim.php
+++ bris_tim.php
@@ -3,14 +3,16 @@ include("sesija_menadzera.php");
 
 $tim_kljuc = $_REQUEST['key'];
 include 'konekcija.php';
-
+//Brisanje imena tima iz tabele ucesce kod korisnika koji su bili raspoređeni u njemu
 $upit = "UPDATE `ucesce` SET `ime_tima` = '' WHERE `ime_tima` = '".$tim_kljuc."'";
 mysqli_query($kon_sa_serv, $upit);
-	
+//Brisanje celog reda iz tabele timovi gde je ime tima $tim_kljuc
 $upit = "DELETE FROM `timovi` WHERE `ime_tima` = '".$tim_kljuc."'";
 mysqli_query($kon_sa_serv, $upit);
 
 mysqli_close($kon_sa_serv);
 echo "<script>location.href = 'vidi_timove.php'</script>";
 ?>
+
+
 
