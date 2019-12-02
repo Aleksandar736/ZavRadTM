149e95a91e54ae4dfa6c0daf0c50a202a5ed2dd4
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-12-02 06:06:50 +0100

    Update obave_clana_o_dod_zad.php

--- obave_clana_o_dod_zad.php
+++ obave_clana_o_dod_zad.php
@@ -2,17 +2,18 @@
 include("sesija_tim_lidera.php");
 
 $ulogov_kor = $_SESSION['korisnicko_ime'];
-
+//Uzima vrednost iz internet adrese .php?key=...
 $kljuc = $_REQUEST['key'];
 include 'konekcija.php';
-
+//Tekst obaveštenja
 $poruka = "Dobili ste zadatak od svog tim lidera. Na stranici Projekti vidite sta je Vas zadatak.";
 $naslov = "obavestenje";
-
+//Upisivanje u bazu u tabelu slanje_poruka
 $upit = "INSERT INTO `slanje_poruka` (`prima`, `salje`, `pregledana_po`, `naslov_poruke`, `tekst_prim_poruke`, `tekst_posl_poruke`)"
 ." VALUES ('".$kljuc."', '".$pri_kor."', 0, '".$naslov."', '".$poruka."', '".$poruka."')";
 mysqli_query($kon_sa_serv, $upit);
-
+//Zatvaranje konekcije
 mysqli_close($kon_sa_serv);
+//Preusmerenje na projekat_tim_lidera.php
 echo "<script>location.href = 'projekat_tim_lidera.php'</script>";
 ?>
