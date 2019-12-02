321671a130ceae0d528528ef26731a7f6ac8bc23
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-12-02 05:42:11 +0100

    Update bris_e_pisma_men.php

--- bris_e_pisma_men.php
+++ bris_e_pisma_men.php
@@ -3,11 +3,13 @@ include("sesija_menadzera.php");
 
 $id_por = $_REQUEST['key'];
 include 'konekcija.php';
-
+//Brisanje iz tabele slanje_poruka reda gde je imejl sa id-jem $id_por
 $upit = "DELETE FROM `slanje_poruka` WHERE `idporuke` = '".$id_por."'";
 mysqli_query($kon_sa_serv, $upit);
 mysqli_close($kon_sa_serv);
 
 echo "<script>location.href = 'primlj_e_pisma_men.php'</script>";
 ?>
+
+
 
