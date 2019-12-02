3f05cf33c4f2051a5eea08ea5f2f452f1689b2b1
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-12-02 06:27:02 +0100

    Update tim_lider.php

--- tim_lider.php
+++ tim_lider.php
@@ -51,7 +51,7 @@ include("sesija_tim_lidera.php");
                     $mess = "";
 
                     include 'konekcija.php';
-
+                    //Broji koliko je ne pregledanih poruka
                     $brojac = 0;
                     $upit = "SELECT * FROM `slanje_poruka` WHERE `prima` = '".$pri_kor."' AND `pregledana` = 0";
                     $result = mysqli_query($kon_sa_serv, $upit);
