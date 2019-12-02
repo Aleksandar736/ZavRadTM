79f27b2706a0d3f946be4718d52a01bd2b232826
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-12-02 06:03:46 +0100

    Update konekcija.php

--- konekcija.php
+++ konekcija.php
@@ -4,12 +4,13 @@ $server = "localhost";
 $user_name = "root";
 $password = "";
 $database = "zavr_rad_tim_me";
-
+//Uspostavljanje konekcije sa serverom i bazom
 $kon_sa_serv = mysqli_connect($server, $user_name, $password);
 $kon_sa_baz = mysqli_select_db($kon_sa_serv, $database);
-
+//Ako nije uspostavljena konekcija sa bazom
 if (!$kon_sa_baz) {
     die("Baza nije pronađena!");
 }
 
 ?>
+
