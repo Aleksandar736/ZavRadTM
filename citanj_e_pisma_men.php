7bfc45987a5b9660820b70662407083729baf435
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-12-02 05:51:43 +0100

    Update citanj_e_pisma_men.php

--- citanj_e_pisma_men.php
+++ citanj_e_pisma_men.php
@@ -49,11 +49,12 @@ include("sesija_menadzera.php");
                 <?php
                 $id_por = $_REQUEST['key'];
                 include 'konekcija.php';
-
+                //Namesta u bazi na 1 (umesto 0) u koloni pregl. (pa se broj neproč. smanjuje za jedan (brojač ga ne broji))
+                //u fajlovima menadzer, tim_lider i obicni_clan
                 $upit = "UPDATE `slanje_poruka` SET `pregledana` = 1 WHERE `idporuke` = '".$id_por."'";
                 mysqli_query($kon_sa_serv, $upit);
 
-
+                //Čita iz baze poruku sa id-jem $id_por
                 $upit = "SELECT * FROM `slanje_poruka` WHERE `idporuke` = '".$id_por."'";
                 $result = mysqli_query($kon_sa_serv, $upit);
                 while ($red = mysqli_fetch_assoc($result)){
@@ -119,7 +120,7 @@ include("sesija_menadzera.php");
                 </tr>            
                 <tr>
                     <td>
-                        <a href = "primlj_e_pisma_men.php"><button>Poštansko sanduče</button></a>
+                        <a href = "primlj_e_pisma_men.php"><button>Primljena <br/>e-pisma</button></a>
                     </td>
                 </tr> 
             </table>
