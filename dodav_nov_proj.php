72be5fec94017eeb4c6658cee6c3666d67dabf18
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-12-02 05:55:15 +0100

    Update dodav_nov_proj.php

--- dodav_nov_proj.php
+++ dodav_nov_proj.php
@@ -44,28 +44,29 @@ include("sesija_menadzera.php");
             </div>
             <div class="tekst-sadrzaja">
                 <?php
-                        echo "<div class='tabelaProPo'>";
-                        echo "<h3 class='dodavanjeNaslov'>Dodaj projekat:</h3>";
-                        echo "</div>";
-                        echo "<div class='tekst-sadrzaja'>";
-                        echo "<fieldset class='fildset2'>";
-                        echo "<form name='add_form' method='post' action='dodav_nov_proj_dod.php'>";
-                        echo "<table border = '0' >";
-                        echo "<tr><td><b>Projekat:</b></td>";
-                        echo "<td colspan='3'><input name = 'ime_proje' type = 'text' size='24' value = ''></td>";
-                        echo "</tr>";
-                        echo "<tr><td style='text-align: right'><b>Opis:</b></td>";
-                        echo "<td colspan='3'><textarea name = 'opis_proje' cols='25' rows='7'></textarea>";
-                        echo "</tr>";
-                        echo "<tr>";
-                        echo "<td></td><td><input name = 'reset' type = 'reset' class='dugmici' value = 'Resetuj'></td>";
-                        echo "<td><input name = 'cancel' type = 'submit' class='dugmici' value = 'Otkaži'></td>";
-                        echo "<td><input name = 'add' type = 'submit' class='dugmici' value = 'Dodaj'></td>";
-                        echo "</tr>";
-                        echo "</table>";
-                        echo "</form>";
-                        echo "</fieldset>";
-                        echo "</div>";
+                //Tabela za dodavanje novog projekta idu ime i opis projekta
+                echo "<div class='tabelaProPo'>";
+                echo "<h3 class='dodavanjeNaslov'>Dodaj projekat:</h3>";//Naslov
+                echo "</div>";
+                echo "<div class='tekst-sadrzaja'>";
+                echo "<fieldset class='fildset2'>";//Forma iz koje podaci idu na dodav_nov_proj_dod.php
+                echo "<form name='add_form' method='post' action='dodav_nov_proj_dod.php'>";
+                echo "<table border = '0' >";
+                echo "<tr><td><b>Projekat:</b></td>";//Ime projekta
+                echo "<td colspan='3'><input name = 'ime_proje' type = 'text' size='24' value = ''></td>";
+                echo "</tr>";
+                echo "<tr><td style='text-align: right'><b>Opis:</b></td>";//Opis projekta
+                echo "<td colspan='3'><textarea name = 'opis_proje' cols='25' rows='7'></textarea>";
+                echo "</tr>";
+                echo "<tr>";
+                echo "<td></td><td><input name = 'reset' type = 'reset' class='dugmici' value = 'Resetuj'></td>";
+                echo "<td><input name = 'cancel' type = 'submit' class='dugmici' value = 'Otkaži'></td>";
+                echo "<td><input name = 'add' type = 'submit' class='dugmici' value = 'Dodaj'></td>";
+                echo "</tr>";
+                echo "</table>";
+                echo "</form>";
+                echo "</fieldset>";
+                echo "</div>";
                 ?>
             </div>        
         </div>
