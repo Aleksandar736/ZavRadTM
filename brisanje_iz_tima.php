64d04e9e244a4e0959978a9e338ffc3f8e601734
Aleksandar736 <56960890+Aleksandar736@users.noreply.github.com>
2019-12-02 05:50:31 +0100

    Update brisanje_iz_tima.php

--- brisanje_iz_tima.php
+++ brisanje_iz_tima.php
@@ -3,19 +3,20 @@ include("sesija_menadzera.php");
 
 $kljuc_del = $_REQUEST['key'];
 include 'konekcija.php';
-
+//Brisanje reda sa podacima izabranog korisnika (koga uklanjamo iz tima) iz tabele ucesce
 $upit = "DELETE FROM `ucesce` WHERE `korisnicko_ime` = '".$kljuc_del."'";
 mysqli_query($kon_sa_serv, $upit);
-
+//Proveravamo da li je izabrani korisnik tim lider
 $upit = "SELECT * FROM `timovi` WHERE `tim_lider` = '".$kljuc_del."'";
 $result = mysqli_query($kon_sa_serv, $upit);
 while ($red = mysqli_fetch_assoc($result)) {
     $ime_tima = $red['ime_tima'];
 }
-
+//Ako jeste bišemo ime te osobe iz kolone tim_lider tabele timovi
 $upit = "UPDATE `timovi` SET `tim_lider` = '' WHERE `ime_tima` = '".$ime_tima."'";
 $result = mysqli_query($upit);
 
 mysqli_close($kon_sa_serv);
 echo "<script>location.href = 'upravljacka_stranica.php'</script>";
 ?>
+
